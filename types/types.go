10dd1664e4d948dabffc2454527880e6c8db2372
Nakama <nakama-202393@202393.local>
2019-08-30 20:07:49 +0700

    Improve API Update delivery and order

--- types/types.go
+++ types/types.go
@@ -2,19 +2,6 @@ package types
 
 import "time"
 
-// StatusDelivery is struct for updating delivery status every city
-type StatusDelivery struct {
-	IDResi int    `json:"IDResi"`
-	Kota   string `json:"Kota"`
-	Status int    `json:"Status"`
-}
-
-//StatusOrder is struct for updating order status
-type StatusOrder struct {
-	IDResi int    `json:"IDResi"`
-	Status string `json:"Status"`
-}
-
 // Delivery is struct for Delivery tabel
 type Delivery struct {
 	IDResi  int       `json:"IDResi"`
