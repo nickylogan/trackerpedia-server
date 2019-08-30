d3aab88266d7aa82867d53272316ce623f3a46b3
Nakama <nakama-202393@202393.local>
2019-08-30 23:29:28 +0700

    Improving All IPA, add new order API

--- types/types.go
+++ types/types.go
@@ -4,20 +4,20 @@ import "time"
 
 // Delivery is struct for Delivery tabel
 type Delivery struct {
-	IDResi  int       `json:"IDResi"`
-	Kota    string    `json:"Kota"`
-	Status  int       `json:"Status"`
-	Ordinal int       `json:"Ordinal"`
-	Time    time.Time `json:"Time"`
+	IDResi  int       `json:"idResi"`
+	Kota    string    `json:"city"`
+	Status  int       `json:"status"`
+	Ordinal int       `json:"ordinal"`
+	Time    time.Time `json:"time"`
 }
 
 // Order is struct for Order Table
 type Order struct {
-	IDOrder            int       `json:"IDOrder"`
-	NamaItem           string    `json:"NamaItem"`
-	Weight             string    `json:"Weight"`
-	Status             string    `json:"Status"`
-	Time               time.Time `json:"Time"`
-	DestinationAddress string    `json:"DestinationAddress"`
-	DestinationCity    string    `json:"DestinationCity"`
+	IDOrder            int       `json:"idOrder"`
+	NamaItem           string    `json:"nameItem"`
+	Weight             string    `json:"weight"`
+	Status             string    `json:"status"`
+	Time               time.Time `json:"time"`
+	DestinationAddress string    `json:"destinationAddress"`
+	DestinationCity    string    `json:"destinationCity"`
 }
