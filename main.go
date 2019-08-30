b2ca3a3938120af16d87cb7d439e9012db84eb8b
Nakama <nakama-202393@202393.local>
2019-08-31 06:34:55 +0700

    Final Commit

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"trackerpedia-server/service"
 
@@ -12,15 +14,18 @@ import (
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", service.Home)
-	router.HandleFunc("/tracker", service.UpdateStatusDelivery).Methods("POST")
+	router.HandleFunc("/tracker", service.UpdateStatusDelivery).Methods("POST", "OPTIONS")
 	router.HandleFunc("/tracker/{id}", service.GetStatusDeliveryByID).Methods("GET")
-	router.HandleFunc("/order", service.UpdateStatusOrder).Methods("POST")
+	router.HandleFunc("/order", service.UpdateStatusOrder).Methods("POST", "OPTIONS")
 	router.HandleFunc("/order/{id}", service.GetStatusOrderByID).Methods("GET")
 	router.HandleFunc("/order_sent", service.GetOrderSent).Methods("GET")
 	router.HandleFunc("/allOrder", service.GetAllOrder).Methods("GET")
-	router.HandleFunc("/newDelivery", service.CreateNewDelivery).Methods("POST")
-	router.HandleFunc("/newOrder", service.CreateNewOrder).Methods("POST")
+	router.HandleFunc("/newDelivery", service.CreateNewDelivery).Methods("POST", "OPTIONS")
+	router.HandleFunc("/newOrder", service.CreateNewOrder).Methods("POST", "OPTIONS")
+	router.HandleFunc("/allItem", service.GetAllItem).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
