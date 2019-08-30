55802da82336abca72064855c9cca7e92da65b2d
alfanjuni <alfanjuni@gmail.com>
2019-08-30 23:06:17 +0700

    API get all order

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	router.HandleFunc("/order", service.UpdateStatusOrder).Methods("POST")
 	router.HandleFunc("/order/{id}", service.GetStatusOrderByID).Methods("GET")
 	router.HandleFunc("/order_sent", service.GetOrderSent).Methods("GET")
+	router.HandleFunc("/allOrder", service.GetAllOrder).Methods("GET")
 	router.HandleFunc("/newDelivery", service.CreateNewDelivery).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
