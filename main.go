c872616f8d8eae549598ec7037856c2b34ebc5cb
Nakama <nakama-202393@202393.local>
2019-08-30 15:44:34 +0700

    restructure the file tracker system

--- main.go
+++ main.go
@@ -1,74 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+
+	"trackerpedia-server/service"
 
 	"github.com/gorilla/mux"
 )
 
-type statusTracker struct {
-	IDResi int32  `json:"IDResi"`
-	Kota   string `json:"Kota"`
-	Status int32  `json:"Status"`
-}
-
-type statusOrder struct {
-	IDResi int    `json:"IDResi"`
-	Status string `json:"Status"`
-}
-
-// Home is function on welcome page
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Tracker System!")
-}
-
-// UpdateStatusTracker is function to update status tracker
-func UpdateStatusTracker(w http.ResponseWriter, r *http.Request) {
-	var response statusTracker
-
-	trackerStatus, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Fprintf(w, "[UpdateStatusTracker] failed to read the json body")
-		return
-	}
-	err = json.Unmarshal(trackerStatus, &response)
-	if err != nil {
-		fmt.Fprintf(w, "[UpdateStatusTracker] failed when unmarshalling json")
-		return
-	}
-
-	json.NewEncoder(w).Encode(response)
-}
-
-// UpdateStatusOrder is function to update status order
-func UpdateStatusOrder(w http.ResponseWriter, r *http.Request) {
-	var response statusOrder
-
-	orderStatus, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Fprintf(w, "[UpdateStatusOrder] failed to read the json body")
-		return
-	}
-
-	err = json.Unmarshal(orderStatus, &response)
-	if err != nil {
-		fmt.Fprintf(w, "[UpdateStatusOrder] failed when unmarshalling json")
-		return
-	}
-
-	json.NewEncoder(w).Encode(response)
-}
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Home)
-	router.HandleFunc("/tracker", UpdateStatusTracker).Methods("POST")
-	router.HandleFunc("/order", UpdateStatusOrder).Methods("POST")
+	router.HandleFunc("/", service.Home)
+	router.HandleFunc("/tracker", service.UpdateStatusTracker).Methods("POST")
+	router.HandleFunc("/order", service.UpdateStatusOrder).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
